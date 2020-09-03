e101403a225f7ccd908b912a6a4c7f6f795f97f7
Gleyton <gleytonclima@gmail.com>
2020-09-02 20:16:53 -0400

    Update to Angular 9

--- src/app/_util/paginator-ptbr-i8n.ts
+++ src/app/_util/paginator-ptbr-i8n.ts
@@ -1,5 +1,7 @@
 import { MatPaginatorIntl } from '@angular/material/paginator';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class MatPaginatorIntlPtBr extends MatPaginatorIntl {
   
     /** A label for the page size selector. */
